2b92f0ae37640608e361464690d6c3f803acc4f8
Ananda Matos <anandamatos@gmail.com>
2025-03-14 17:32:13 -0300

    // Captura os elementos do DOM

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Array para armazenar os nomes dos amigos
+let amigos = [];
+
+// Captura os elementos do DOM
+const inputNome = document.querySelector("#amigo");
+const listaAmigos = document.querySelector("#listaAmigos");
+const resultadoSorteio = document.querySelector("#resultado");
